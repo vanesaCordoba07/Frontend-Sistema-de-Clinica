b544b94c3f5da11850de5d0ae1b44d04d5779d83
paulip13 <paulip2713@gmail.com>
2026-04-16 13:05:00 -0500

    Fix: corregir rutas de imports en servicios y modelos

--- web/src/app/core/services/servicio.service.ts
+++ web/src/app/core/services/servicio.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ServicioRead, ServicioCreate, ServicioUpdate } from '../models/api.models';
+import { ServicioRead, ServicioCreate, ServicioUpdate } from '../../models/api.models';
 
 @Injectable({
     providedIn: 'root'
